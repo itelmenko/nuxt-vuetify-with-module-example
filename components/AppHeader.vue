a85af97a5eab065264ed27e8c7c47aa5c31d8fca
Igor Telmenko <igor-telmenko@yandex.ru>
2024-03-21 08:03:43 +0300

    Добавлены страницы и Layout

---
+++ components/AppHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    Header [<NuxtLink to="/">Main</NuxtLink>] [<NuxtLink to="/about"
+      >About</NuxtLink
+    >]
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert> This is an AppAlert</AppAlert>
+  </div>
+</template>
