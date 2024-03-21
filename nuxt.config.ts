9146f66568ac5d25704ef3082d31148ffecced7b
Igor Telmenko <igor-telmenko@yandex.ru>
2024-03-21 08:34:59 +0300

    Кастомизация Vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,61 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { fileURLToPath } from "url";
+import { defineNuxtConfig } from "nuxt/config";
+import { defineVuetifyConfiguration } from "vuetify-nuxt-module/custom-configuration";
+
+const customDarkTheme = {
+  dark: true,
+  colors: {
+    primary: "#f74780",
+    error: "#e74c3c",
+  },
+};
+
+const customLightTheme = {
+  dark: false,
+  colors: {
+    primary: "#f74780",
+    error: "#e74c3c",
+  },
+};
+
+const vuetifyConfig = defineVuetifyConfiguration({
+  theme: {
+    defaultTheme: "custom-light",
+    themes: {
+      "custom-dark": customDarkTheme,
+      "custom-light": customLightTheme,
+    },
+  },
+  defaults: {
+    VTextField: {
+      variant: "outlined",
+    },
+    VSelect: {
+      variant: "outlined",
+    },
+    VTextarea: {
+      variant: "outlined",
+    },
+  },
+});
+
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  alias: {
+    "@": fileURLToPath(new URL("./", import.meta.url)),
+  },
+  typescript: {
+    typeCheck: true,
+  },
   modules: ["vuetify-nuxt-module"],
   vuetify: {
     moduleOptions: {
-      /* module specific options */
+      styles: {
+        configFile: "assets/css/settings.scss",
+      },
     },
-    vuetifyOptions: {
-      /* vuetify options */
-    },
+    vuetifyOptions: vuetifyConfig,
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     <h1>Welcome to the homepage</h1>
-    <AppAlert> This is an AppAlert</AppAlert>
+
+    <h2>Vuetify</h2>
+
+    <v-text-field placeholder="Example of textfield"></v-text-field>
+
+    <v-select
+      placeholder="Select a state"
+      :items="[
+        'California',
+        'Colorado',
+        'Florida',
+        'Georgia',
+        'Texas',
+        'Wyoming',
+      ]"
+    ></v-select>
+
+    <v-textarea placeholder="Input your comment here"></v-textarea>
   </div>
 </template>
